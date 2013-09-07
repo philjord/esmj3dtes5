dddcad7cc7e4c6af73d375ebf08ec3d4002a50c9
philip <philip@ihug.co.nz>
2013-09-08 09:36:20 +1200

    .

--- esmj3dtes5/src/esmj3dtes5/data/records/REFR.java
+++ esmj3dtes5/src/esmj3dtes5/data/records/REFR.java
@@ -2,51 +2,20 @@ package esmj3dtes5.data.records;
 
 import java.util.ArrayList;
 
-import utils.ESMByteConvert;
 import esmLoader.common.data.record.Record;
 import esmLoader.common.data.record.Subrecord;
-import esmj3d.data.shared.records.InstRECO;
-import esmj3d.data.shared.subrecords.FormID;
-import esmj3d.data.shared.subrecords.XTEL;
-import esmj3d.data.shared.subrecords.ZString;
-import esmj3dtes5.data.subrecords.FNAM;
-import esmj3dtes5.data.subrecords.TNAM_c;
+import esmj3d.data.shared.records.CommonREFR;
 import esmj3dtes5.data.subrecords.XACT;
 import esmj3dtes5.data.subrecords.XCNT;
 import esmj3dtes5.data.subrecords.XESP;
 import esmj3dtes5.data.subrecords.XLCM;
-import esmj3dtes5.data.subrecords.XLOC;
-import esmj3dtes5.data.subrecords.XLOD;
 import esmj3dtes5.data.subrecords.XPCI;
-import esmj3dtes5.data.subrecords.XRNK;
 import esmj3dtes5.data.subrecords.XSED;
 
-public class REFR extends InstRECO
+public class REFR extends CommonREFR
 {
 
-	public ZString EDID;
-
-	public FormID NAME;
-
-	public boolean XMRK = false;
-
-	public FNAM FNAM;
-
-	public FormID XOWN;
-
-	public XRNK XRNK;
-
-	public FormID XGLB;
-
-	public XTEL XTEL;
-
-	public FormID XTRG;
-
 	public XSED XSED;
-
-	public XLOD XLOD;
-
-	public XLOC XLOC;
 
 	public XPCI XPCI;
 
@@ -54,17 +23,9 @@ public class REFR extends InstRECO
 
 	public XLCM XLCM;
 
-	public FormID XRTM;
-
 	public XACT XACT;
 
 	public XCNT XCNT;
-
-	public FormID FULL;
-
-	public TNAM_c TNAM;
-
-	public boolean ONAM = false;
 
 	public REFR(Record recordData)
 	{
@@ -76,61 +37,13 @@ public class REFR extends InstRECO
 			Subrecord sr = subrecords.get(i);
 			byte[] bs = sr.getSubrecordData();
 
-			if (sr.getSubrecordType().equals("EDID"))
-			{
-				EDID = new ZString(bs);
-			}
-			else if (sr.getSubrecordType().equals("NAME"))
-			{
-				NAME = new FormID(bs);
-			}
-			else if (sr.getSubrecordType().equals("XMRK"))
-			{
-				XMRK = true;
-			}
-			else if (sr.getSubrecordType().equals("FNAM"))
-			{
-				FNAM = new FNAM(bs);
-			}
-			else if (sr.getSubrecordType().equals("XOWN"))
-			{
-				XOWN = new FormID(bs);
-			}
-			else if (sr.getSubrecordType().equals("XRNK"))
-			{
-				XRNK = new XRNK(bs);
-			}
-			else if (sr.getSubrecordType().equals("XGLB"))
-			{
-				XGLB = new FormID(bs);
-			}
-			else if (sr.getSubrecordType().equals("XSCL"))
-			{
-				scale = ESMByteConvert.extractFloat(bs, 0);
-			}
-			else if (sr.getSubrecordType().equals("XTEL"))
-			{
-				XTEL = new XTEL(bs);
-			}
-			else if (sr.getSubrecordType().equals("XTRG"))
-			{
-				XTRG = new FormID(bs);
-			}
-			else if (sr.getSubrecordType().equals("XSED"))
+			if (sr.getSubrecordType().equals("XSED"))
 			{
 				XSED = new XSED(bs);
-			}
-			else if (sr.getSubrecordType().equals("XLOD"))
-			{
-				XLOD = new XLOD(bs);
 			}
 			else if (sr.getSubrecordType().equals("XPCI"))
 			{
 				XPCI = new XPCI(bs);
-			}
-			else if (sr.getSubrecordType().equals("XLOC"))
-			{
-				XLOC = new XLOC(bs);
 			}
 			else if (sr.getSubrecordType().equals("XESP"))
 			{
@@ -140,10 +53,6 @@ public class REFR extends InstRECO
 			{
 				XLCM = new XLCM(bs);
 			}
-			else if (sr.getSubrecordType().equals("XRTM"))
-			{
-				XRTM = new FormID(bs);
-			}
 			else if (sr.getSubrecordType().equals("XACT"))
 			{
 				XACT = new XACT(bs);
@@ -151,22 +60,6 @@ public class REFR extends InstRECO
 			else if (sr.getSubrecordType().equals("XCNT"))
 			{
 				XCNT = new XCNT(bs);
-			}
-			else if (sr.getSubrecordType().equals("FULL"))
-			{
-				FULL = new FormID(bs);
-			}
-			else if (sr.getSubrecordType().equals("TNAM"))
-			{
-				TNAM = new TNAM_c(bs);
-			}
-			else if (sr.getSubrecordType().equals("ONAM"))
-			{
-				ONAM = true;
-			}
-			else if (sr.getSubrecordType().equals("DATA"))
-			{
-				this.extractInstData(bs);
 			}
 			else if (sr.getSubrecordType().equals("VMAD"))
 			{
@@ -281,10 +174,10 @@ public class REFR extends InstRECO
 			}
 			else
 			{
-				System.out.println("unhandled : " + sr.getSubrecordType() + " in " + recordData);
+				//CommonREFR makes this no use now
+				//	System.out.println("unhandled : " + sr.getSubrecordType() + " in " + recordData);
 			}
 		}
 
 	}
-
 }
