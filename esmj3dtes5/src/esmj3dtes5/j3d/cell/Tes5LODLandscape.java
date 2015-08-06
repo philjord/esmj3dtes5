e1fd71b5e55dd0c0d5c1245c1d9d9206b1ac0992
phil <phil@Thinky>
2015-08-06 19:28:09 +0200

    Lod loading much improved

--- esmj3dtes5/src/esmj3dtes5/j3d/cell/Tes5LODLandscape.java
+++ esmj3dtes5/src/esmj3dtes5/j3d/cell/Tes5LODLandscape.java
@@ -11,8 +11,14 @@ import javax.vecmath.Vector3f;
 import nif.NiObjectList;
 import nif.NifFile;
 import nif.NifToJ3d;
+import nif.basic.NifRef;
 import nif.j3d.J3dNiTriShape;
+import nif.niobject.NiAVObject;
+import nif.niobject.NiObject;
+import nif.niobject.NiTriShape;
 import nif.niobject.NiTriShapeData;
+import nif.niobject.bs.BSLightingShaderProperty;
+import nif.niobject.bs.BSMultiBoundNode;
 import nif.niobject.bs.BSShaderTextureSet;
 import utils.source.MeshSource;
 import utils.source.TextureSource;
@@ -33,31 +39,97 @@ public class Tes5LODLandscape extends MorphingLandscape
 			if (nf != null)
 			{
 				NiObjectList blocks = nf.blocks;
-
-				// we know it is a NiTriShapeData at block 2
-				NiTriShapeData data = (NiTriShapeData) blocks.getNiObjects()[2];
-
-				//scale 4 will get morph treatment later
-				boolean morphable = (scale == 4);
-				IndexedGeometryArray baseItsa = J3dNiTriShape.createGeometry(data, morphable);
-
-				if (morphable)
-				{
-					this.setGeometryArray(baseItsa);
-				}
-
-				Shape3D shape = new Shape3D();
-				shape.setGeometry(baseItsa);
-
-				//we know it's a textureset at block 4
-				BSShaderTextureSet ts = (BSShaderTextureSet) blocks.getNiObjects()[4];
-				shape.setAppearance(createAppearance(textureSource.getTexture(ts.textures[0])));
+				BSMultiBoundNode root = (BSMultiBoundNode) blocks.root();
 
 				TransformGroup tg = new TransformGroup();
 				Transform3D t = new Transform3D(new Quat4f(0, 0, 0, 1), new Vector3f((lodX * J3dLAND.LAND_SIZE), 0,
 						(-lodY * J3dLAND.LAND_SIZE)), scale);
 				tg.setTransform(t);
-				tg.addChild(shape);
+
+				for (NifRef cnr : root.children)
+				{
+					NiAVObject child = (NiAVObject) nf.blocks.get(cnr);
+
+					if (child instanceof NiTriShape)
+					{
+						// regular lod terrains
+						NiTriShape niTriShape = (NiTriShape) child;
+						NiTriShapeData data = (NiTriShapeData) blocks.get(niTriShape.data);
+
+						//scale 4 will get morph treatment later
+						boolean morphable = (scale == 4);
+						IndexedGeometryArray baseItsa = J3dNiTriShape.createGeometry(data, morphable);
+
+						if (morphable)
+						{
+							this.addGeometryArray(baseItsa);
+						}
+
+						Shape3D shape = new Shape3D();
+						shape.setGeometry(baseItsa);
+
+						BSLightingShaderProperty lp = getLightingProperty(niTriShape, blocks);
+						if (lp != null)
+						{
+							BSShaderTextureSet ts = (BSShaderTextureSet) blocks.get(lp.TextureSet);
+							shape.setAppearance(createAppearance(textureSource.getTexture(ts.textures[0])));
+							tg.addChild(shape);
+						}
+						else
+						{
+							System.out.println("unpropertied trishape in skyrim lod " + niTriShape + " in " + meshName);
+						}
+					}
+					else if (child instanceof BSMultiBoundNode)
+					{
+						//Skyrim has water under it's own bounds node
+						BSMultiBoundNode waterRoot = (BSMultiBoundNode) child;
+
+						for (NifRef wcnr : waterRoot.children)
+						{
+							NiAVObject waterChild = (NiAVObject) nf.blocks.get(wcnr);
+
+							if (waterChild instanceof NiTriShape)
+							{
+								// regular lod terrains
+								NiTriShape niTriShape = (NiTriShape) waterChild;
+								NiTriShapeData data = (NiTriShapeData) blocks.get(niTriShape.data);
+
+								//scale 4 will get morph treatment later
+								boolean morphable = (scale == 4);
+								IndexedGeometryArray baseItsa = J3dNiTriShape.createGeometry(data, morphable);
+
+								if (morphable)
+								{
+									this.addGeometryArray(baseItsa);
+								}
+
+								Shape3D shape = new Shape3D();
+								shape.setGeometry(baseItsa);
+
+								BSLightingShaderProperty lp = getLightingProperty(niTriShape, blocks);
+								if (lp != null)
+								{
+									System.out.println("Skyrim water child has properties! " + waterChild + " in " + meshName);
+								}
+								else
+								{
+									shape.setAppearance(createBasicWaterApp());
+									tg.addChild(shape);
+								}
+							}
+							else
+							{
+								System.out.println("Lod file water child odd " + waterChild + " in " + meshName);
+							}
+						}
+					}
+					else
+					{
+						System.out.println("Lod file child odd " + child + " in " + meshName);
+					}
+				}
+
 				addChild(tg);
 			}
 			else
@@ -70,5 +142,16 @@ public class Tes5LODLandscape extends MorphingLandscape
 			//fine the systems just asking for empty space which is fine
 			//System.out.println("Bad landscape name " + meshName);
 		}
+	}
+
+	private BSLightingShaderProperty getLightingProperty(NiTriShape niTriShape, NiObjectList blocks)
+	{
+		for (NifRef pnr : niTriShape.properties)
+		{
+			NiObject p = blocks.get(pnr);
+			if (p instanceof BSLightingShaderProperty)
+				return (BSLightingShaderProperty) p;
+		}
+		return null;
 	}
 }
